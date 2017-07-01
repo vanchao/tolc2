669725124f35c1b512c76b3e1f38c885becfb788
fanchao <van.chao@hotmail.com>
2017-07-01 11:15:37 +0800

    Create application with test case

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class IndexTest extends TestCase
+{
+    public function testHello()
+    {
+        $_GET['name'] = 'Fabien';
+        ob_start();
+        include 'index.php';
+        $content = ob_get_clean();
+        $this->assetEquals('Hello Fabien', $content);
+    }
+}
