d3671ff60c81f5d8292cfbdbd6f7fcdf78f556ec
fanchao <van.chao@hotmail.com>
2017-07-01 17:48:51 +0800

    Use Symfony-Compoent to refactor index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
-$input = isset($_GET['name']) ? $_GET['name'] : 'World' ;
-header('Content-Type: text/html; charset=utf-8');
-printf('Hello %s', htmlspecialchars( $input, ENT_QUOTES, 'UTF-8' ));
+require_once __DIR__ . '/vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+$request = Request::createFromGlobals();
+$input = $request->get('name', 'World');
+$response = new Response(sprintf('Hello %s', htmlspecialchars( $input, ENT_QUOTES, 'UTF-8' )));
+$response->send();
