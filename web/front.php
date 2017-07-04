725b813fabb940cf7e83ef3cb54060e14ab58aa1
fanchao <van.chao@hotmail.com>
2017-07-04 21:05:40 +0800

    Separate controller model view

--- web/front.php
+++ web/front.php
@@ -20,14 +20,6 @@ $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 $controllerResolver = new HttpKernel\Controller\ControllerResolver();
 $argumentResolver = new HttpKernel\Controller\ArgumentResolver();
-try {
-    $request->attributes->add($matcher->match($request->getPathInfo()));
-    $controller = $controllerResolver->getController($request);
-    $arguments = $argumentResolver->getArguments($request, $controller);
-    $response = call_user_func_array($controller, $arguments);
-} catch(Routing\Exception\ResourceNotFoundException $e) {
-    $response = new Response('Not Found', 404);
-} catch(Exception $e) {
-    $response = new Response('An error occurred', 500);
-}
+$framework = new Clot\Framework($matcher, $controllerResolver, $argumentResolver);
+$response = $framework->handle($request);
 $response->send();
